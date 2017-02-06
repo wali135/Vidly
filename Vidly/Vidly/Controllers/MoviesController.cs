f3c03f6c4c3819c2a22c5d49666b1a00e0ad99e2
Khurshid Wali <khurshid135@hotmail.com>
2017-02-07 02:23:36 +0500

    4.Building Form

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Vidly.Migrations;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -15,71 +17,99 @@ namespace Vidly.Controllers
         {
             _context = new ApplicationDbContext();
         }
+
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
         }
 
-        public ActionResult Index()
+        public ViewResult Index()
         {
-
-            var movies = _context.Movies.ToList();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-
-        public ActionResult New()
+        public ViewResult New()
         {
-            var movies = _context.Movies.ToList();
-            return View("Movieform");
+            var genres = _context.Genres.ToList();
 
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+
+        }
+
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
         [HttpPost]
         public ActionResult Save(Movie movie)
         {
             if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
                 _context.Movies.Add(movie);
+            }
             else
             {
-                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
-
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
                 movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
                 movieInDb.ReleaseDate = movie.ReleaseDate;
-                movieInDb.Genre = movie.Genre;
-                movieInDb.Stock = movie.Stock;
-                movieInDb.DateAdded = movie.DateAdded;
-
             }
+
             _context.SaveChanges();
+
             return RedirectToAction("Index", "Movies");
         }
-
-        public ActionResult Random(int? id)
-        {
-            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
-            if (movie == null)
-                return HttpNotFound();
-            
-
-            return View(movie);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
-            
-                if (movie == null)
-                    return HttpNotFound();
-
-            
-               
-
-            return View("Movieform",movie);
-
-        }
-
-        
     }
 }
