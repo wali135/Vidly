f68530757dbf40f7b3a4b9e4acec4419c80ac6ee
Khurshid Wali <khurshid135@hotmail.com>
2017-02-07 03:24:13 +0500

    Commit Site.css and Language Layout

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -9,6 +9,7 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
